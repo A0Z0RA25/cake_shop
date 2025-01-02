372e0d759ee344645fa6ac39a9eda0ec2d3ed5a7
Carl Jasper Ramos <carljasperramos97@gmail.com>
2025-01-02 10:48:27 +0800

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain="dev-7smbydghn55vazmj.us.auth0.com"
     clientId="RxvWrc6Y0fwuwLzwo29wjdBhqtj6MTfo"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
     }}
   >
-    <App />
-  </Auth0Provider>,
-)
+    <Router>
+      <App />
+    </Router>
+  </Auth0Provider>
+);
