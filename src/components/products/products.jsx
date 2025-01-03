400975a0c9eb1c6066b97edad257ee071a6bd85c
Carl Jasper Ramos <carljasperramos97@gmail.com>
2025-01-03 13:46:52 +0800

    new-changes

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -2,7 +2,7 @@ import { cakeContainer } from '../productsCon/productsCon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import ribbon from './ribbon.png';
+import banner from './banner.png';
 import special from './special.png';
 import { useState } from 'react';
 
@@ -13,7 +13,7 @@ function Products({ handleAddCart }){
     const [cakeCat, setCakeCat] = useState(cakeContainer);
     const [findCake, setFindCake] = useState(null)
     const [heartColor, setHeartColor] = useState(false);
-    const [displayCat, setDisplayCat] = useState("Categories");
+    const [displayCat, setDisplayCat] = useState(false);
 
     function handleCat(e){
         if(e === "All"){
@@ -24,7 +24,6 @@ function Products({ handleAddCart }){
             const filterCat = cakeContainer.filter((cat) => {
                 return cat.type === catSpace
             })
-            setDisplayCat(e)
             setCakeCat(filterCat) 
         } 
     }
@@ -54,33 +53,37 @@ function Products({ handleAddCart }){
     }
 
     return(
-        <div id='cake-container' className="w-full min-h-svh h-full">
+        <div id='cake-container' className="w-full min-h-svh h-full bg-productBg py-10">
             {/* products container */}
-            <div className='md:flex flex-row justify-around md:px-3 w-full'>
+            <div className='md:flex flex-row justify-around md:px-3 w-full grid grid-cols-1'>
             {/* list of category */}
-            <div className='h-full grid md:grid-rows-3 grid-cols-2 md:mt-5 gap-y-5 gap-x-2 md:p-0 p-2'>
-                <div className='border relative border-black md:h-40 md:w-56 w-full'>
+            <div className='h-full grid md:grid-rows-1 md:grid-cols-1 grid-cols-1 md:mt-5 gap-y-5 gap-x-2 md:p-0 p-2'>
+                {/* Search Bar */}
+                <div>
+                    <input placeholder='Search product' id='searchProduct' onKeyUp={(e) => handleSearchCake(e)} className="border text-xs md:w-1/2 w-[150px] p-1 border-black" type="text" />
+                </div>
+                {/* list */}
+                <div className='border relative border-black md:h-full md:w-56 w-full hidden md:block'>
                     <div className='relative'>
-                        <img className='absolute -top-[41.5px] h-24 w-full -z-10' src={ribbon} alt="" />
-                        <h1 className='text-center text-white'>Special Cake</h1>
+                        <h1 className='bg-banner text-white w-[70%] text-center rounded-br-md'>Categories</h1>
                     </div>
-                    <img className='h-24 w-36 mx-auto' src={special} alt="" />
-                    {/* name and price */}
-                    <div className='flex md:flex-row flex-col text-center justify-evenly'>
-                        <h1 className='bg-banner text-sm px-2'>Chocolate cake</h1>
-                        <p>500 pesos</p>
+                    <div className="group text-right overflow-hidden">
+                        <ul className='z-20'>
+                            {productCat.map((eachCat, index) => (
+                                <li key={index} onClick={() => handleCat(eachCat)} className='text-sm md:w-[100%] md:-translate-x-20 hover:translate-x-0 ease-in-out duration-500 border rounded-r-xl bg-banner text-white my-1 border-black md:px-5 px-2 py-2'>{eachCat}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
-                <div className='border relative border-black md:h-40 md:w-56 w-full'>
+                {/* specials */}
+                <div className='border relative border-black md:h-full md:w-56 w-full hidden md:block'>
                     <div className='relative'>
-                        <img className='absolute -top-[41.5px] h-24 w-full -z-10' src={ribbon} alt="" />
-                        <h1 className='text-center text-white'>Special Cake</h1>
+                        <h1 className='bg-banner text-white w-[70%] text-center rounded-br-md'>Special cake</h1>
                     </div>
-                    <img className='h-24 w-36 mx-auto' src={special} alt="" />
+                    <img className='h-24 w-36 mx-auto my-1' src={special} alt="" />
                     {/* name and price */}
                     <div className='flex md:flex-row flex-col text-center justify-evenly'>
-                        <h1 className='bg-banner text-sm px-2'>Chocolate cake</h1>
-                        <p>500 pesos</p>
+                        <h1 className='bg-banner rounded-t-sm text-white px-2'>Chocolate cake</h1>
                     </div>
                 </div>
             </div>
@@ -90,28 +93,15 @@ function Products({ handleAddCart }){
                 {/* Products Header */}
                 <div className="md:mt-5 flex md:justify-around flex-col md:flex-row justify-center items-center gap-y-2">
                     <h1 className="md:text-2xl">Featured</h1>
-                    {/* categories */}
-                    <div className="group md:w-[180px] text-center border border-black">
-                        <h1 className='px-6'>{displayCat}</h1>
-                        <ul className='group-hover:block hidden absolute bg-white shadow-xl z-20'>
-                            {productCat.map((eachCat, index) => (
-                                <li key={index} onClick={() => handleCat(eachCat)} className='hover:bg-gray-100 md:w-[180px] md:px-5 px-2 py-2'>{eachCat}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    {/* Search Bar */}
-                    <div>
-                        <input placeholder='Search product' id='searchProduct' onKeyUp={(e) => handleSearchCake(e)} className="border p-1 border-black" type="text" />
-                    </div>
                 </div>
                 {findCake ? 
                 /* Not found */
                 (<div className='md:text-2xl flex justify-center items-center text-center mt-20'>{findCake}</div>) :
                 (<div className='grid md:grid-cols-3 grid-cols-2 md:gap-x-5 md:gap-y-8 gap-2 md:p-0 p-2 items-center justify-evenly md:mt-5'>
                     {cakeCat.map((cake, index) => (
-                    <div key={index} className='relative shadow-md md:px-3 px-1 py-3 rounded-md text-center '>
+                    <div key={index} className='relative hover:-translate-y-3 duration-500 ease-in-out bg-banner text-white shadow-md md:px-3 px-1 py-3 rounded-md text-center '>
                         {/* add to favorite */}
-                         <button onClick={() => handleFav(index)}><FontAwesomeIcon className={`absolute left-5  ${heartColor[index] ? 'text-red-500' : 'text-black'}`} icon={faHeart} /></button>
+                         <button onClick={() => handleFav(index)}><FontAwesomeIcon className={`absolute left-5 text-white  ${heartColor[index] ? 'text-red-500' : 'text-black'}`} icon={faHeart} /></button>
                         <img className='md:h-[100px] md:w-[150px] h-[100px] w-[130px] mx-auto' src={cake.img} alt="" />
                         {/* Price */}
                         <div className=''>
@@ -120,7 +110,7 @@ function Products({ handleAddCart }){
                         </div>
                         {/* Buy Btn */}
                         <div className='flex gap-2 justify-center md:mt-3'>
-                            <button className='bg-red-500 md:px-5 px-2 rounded-md text-white'>Order Now</button>
+                            <button className='hover:scale-105 duration-300 ease-in-out border bg-white md:px-5 px-2 rounded-md text-black'>Order Now</button>
                             <button className='hover:scale-110 duration-300 ease-in-out' onClick={() => handleAddCart(cake)}><FontAwesomeIcon icon={faCartShopping} /></button>
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,16 @@ export default {
     extend: {
       colors: { 
         blue: "#C6E7FF",
-        banner: "#FFE6A9",
-        color: "#FBFBFB",
+        banner: "#543A14",
+        homeBg: "#F0BB78",
+        productBg: "#ede9e2",
+        tColor: "#FFF0DC",
         btn: "#FFDDAE",
+      },
+      fontFamily: {
+        header: ['Poppins', 'sans-serif'],
+        text: ['Quicksand', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'],
       },
     },
   },
